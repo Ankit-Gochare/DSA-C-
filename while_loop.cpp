c4754eead6df8caf01b17bccc0dc69a74bee18c6
Ankit Gochare <ankitgochare35@gmail.com>
2024-12-07 20:44:51 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// while loop syntax :-
+
+//  initialize;
+//  while(break )   
+//  {
+//   ghjegdyuewgfiuwhfuywrg
+//   hdgewufriuhfuywgfuewh
+
+//   update;
+
+//  };
+
+ 
+
+//  //print numbers 1 to 10
+
+//  int i=1;
+//  while(i<=10)
+//  {
+//    cout<<i <<"  ";
+
+//    i=i+1;
+//  }
+//  cout<<endl;
+ 
+
+//  //table
+//  int j, n;
+
+//  cout<<"enter the number : ";
+//  cin>>n;
+
+//  j=1;
+//  while(j<=10)
+//  {
+//     cout<<n<<"*"<<j<<"= "<<j*n<<endl;
+
+//     j=j+1;
+//  }
+ 
+//  //factors
+//  int k, number;
+//  cout<<"enter the number : ";
+//  cin>>number;
+//  k=1;
+//  while(k<=number)
+//  {
+//     if(number%k==0)
+//     cout<<k<<" ";
+
+//     k++;
+//  }
+ 
+
+//  //even  numbers
+//  int i , n;
+
+//  cout<<"enter the last number : ";
+//  cin>>n;
+
+//  i=1;
+//  while(i<=n)
+//  {
+//     if(i%2==0)
+//     cout<<i<<" ";
+
+//     i++;
+//  }
+//  cout<<endl;
+
+
+ //odd number
+ int l, num;
+
+ cout<<"enter the number : ";
+ cin>>num;
+
+ l=1;
+ while(l<=num)
+ {
+    if(l%2==1)
+    cout<<l<<" ";
+
+    l++;
+ }
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
