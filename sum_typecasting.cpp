66617b8f93999dcee64ab9177b0034a39cb4a9e9
Ankit Gochare <ankitgochare35@gmail.com>
2024-12-07 18:43:11 +0530

    Add files via upload

---
+++ sum_typecasting.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //sum of two numbers
+// int a ,b ;
+//  cout<<"enter the first number :";
+//  cin>>a;
+
+//  cout<<"enter the second number :";
+//  cin>>b;
+
+//  cout<<"sum of the two numbers :"<<a+b ; 
+
+   /*// type casting :- kisi type ke data ko kisi or type me store karana 
+    //jaise kisi character ko integer me store karana tb vh uski ascii value ko store kar leta h . 
+    //or agar kisi character me kisi integer ko store kiya jay to vo ascii me us no. pe jo hoga usko store kr lega.
+    // bade bit k type ko chhote bit me dalege to data lose hota h.
+    
+    agar hame kbhi kisi character ki vaue pata karna ho to hum type casting se pta kar skte h.
+     */
+
+    // int a = 20;
+    // char c = 'b';
+    // a = c;
+    // cout<<a;
+   
+
+// int a = 65;
+//     char c = 'd';
+//     c = a;
+//     cout<<c;
+}
