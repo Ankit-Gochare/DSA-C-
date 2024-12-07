b81b5e2dd6fe77e403fbb1ad2874ebb03656859e
Ankit Gochare <ankitgochare35@gmail.com>
2024-12-07 20:46:41 +0530

    Add files via upload

---
+++ do_while_loop.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+// // do while loop :-
+// syntax :- 
+// initialize;
+// to do ; 
+// update ;
+// break;
+
+
+// // sum of n natural numbers
+// int i , sum=0 , n;
+
+// cout<<"enter the last number : ";
+// cin>>n;
+
+// i=1;
+// do{
+//   sum=sum+i;  
+//  cout<<sum<<" ";
+
+//  i++;
+// }
+// while(i<=n);
+
+// cout<<endl;
+
+// //counting
+// int j , num ;
+
+// cout<<"enter the number : ";
+// cin>>num;
+
+// j=1;
+// do{
+//   cout<<j<<" ";
+
+//   j++;
+// }
+// while(j<=num);
+
+// cout<<endl;
+
+
+
+//table
+int k, j,numb ;
+
+cout<<"enter the number : ";
+cin>>numb;
+
+j=1;
+do{
+  cout<<numb<<"*"<<j<<" = "<<numb*j<<endl;
+
+  j++;
+}
+while(j<=10);
+
+cout<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
